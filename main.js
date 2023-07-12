64d8f9788fa1c9c84aaf1ac574ddabaed808cf6c
Felipe Silva <felipems_16@hotmail.com>
2023-07-11 19:40:36 -0500

    primeros archivos del proyecto

---
+++ main.js
@@ -0,0 +1,21 @@
+const h1 = document.querySelector('h1');
+const formu = document.querySelector('#form')
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResultado = document.querySelector('#resultado');
+
+
+form.addEventListener('submit', sumarInputValues);
+
+function sumarInputValues(event) {
+    event.preventDefault();
+    const sumaInpunts = input1.value + input2.value;
+    pResultado.innerText = "Resultado: " + sumaInpunts;
+    
+}
+
+
+
+
+
